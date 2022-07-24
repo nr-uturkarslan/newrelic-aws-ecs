98715a9d221d0388a202e1fe7d84581a4b96491a
uturkarslan <ugurturkarslan@gmail.com>
2022-07-24 17:39:44 +0200

    Implement s3

--- apps/validation/src/main/java/com/newrelic/aws/validation/service/validate/ValidateCustomItemService.java
+++ apps/validation/src/main/java/com/newrelic/aws/validation/service/validate/ValidateCustomItemService.java
@@ -1,14 +1,25 @@
 package com.newrelic.aws.validation.service.validate;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.google.gson.Gson;
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.aws.validation.dto.ResponseDto;
 import com.newrelic.aws.validation.entity.ValidationResult;
 import com.newrelic.aws.validation.service.validate.dto.InvalidReason;
 import com.newrelic.aws.validation.service.validate.dto.ValidateRequestDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -16,6 +27,11 @@ import java.util.UUID;
 public class ValidateCustomItemService {
 
     private final Logger logger = LoggerFactory.getLogger(ValidateCustomItemService.class);
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    private final Gson gson = new Gson();
 
     public ValidateCustomItemService() {}
 
@@ -28,6 +44,9 @@ public class ValidateCustomItemService {
         validationResult.setValidationId(UUID.randomUUID().toString());
         validationResult.setCustomItemInvalidReasons(new ArrayList<>());
 
+        var trace = NewRelic.getAgent().getTracedMethod();
+        trace.addCustomAttribute("customItemValidationId", validationResult.getValidationId());
+
         // Name
         validateCustomItemName(validateRequestDto, validationResult);
 
@@ -37,19 +56,22 @@ public class ValidateCustomItemService {
         // Request Timestamp
         validateCustomItemRequestTimestamp(validateRequestDto, validationResult);
 
+        validationResult.setCustomItemValidationTimestamp(
+                new Timestamp(System.currentTimeMillis()).toString()
+        );
         logger.info("message:Custom item is validated.");
 
         var responseDto = new ResponseDto<ValidationResult>();
         responseDto.setData(validationResult);
 
         if (validationResult.getCustomItemInvalidReasons().isEmpty()) {
-            logger.info("message:Custom is valid.");
-            responseDto.setMessage("Custom is valid.");
+            logger.info("message:Custom item is valid.");
+            responseDto.setMessage("Custom item is valid.");
             return new ResponseEntity<>(responseDto, HttpStatus.ACCEPTED);
         }
         else {
-            logger.warn("message:Custom is invalid.");
-            responseDto.setMessage("Custom is invalid.");
+            logger.warn("message:Custom item is invalid.");
+            responseDto.setMessage("Custom item is invalid.");
             return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
         }
     }
@@ -64,6 +86,8 @@ public class ValidateCustomItemService {
                     "validationId:" + validationResult.getValidationId());
             validationResult.getCustomItemInvalidReasons()
                     .add(InvalidReason.NAME_NOT_PROVIDED.getValue());
+            var trace = NewRelic.getAgent().getTracedMethod();
+            trace.addCustomAttribute("invalidReasonNameNotProvided", true);
         }
     }
 
@@ -77,6 +101,8 @@ public class ValidateCustomItemService {
                     "validationId:" + validationResult.getValidationId());
             validationResult.getCustomItemInvalidReasons()
                     .add(InvalidReason.DESCRIPTION_NOT_PROVIDED.getValue());
+            var trace = NewRelic.getAgent().getTracedMethod();
+            trace.addCustomAttribute("invalidReasonDescriptionNotProvided", true);
         }
     }
 
@@ -90,6 +116,39 @@ public class ValidateCustomItemService {
                     "validationId:" + validationResult.getValidationId());
             validationResult.getCustomItemInvalidReasons()
                     .add(InvalidReason.REQUEST_TIMESTAMP_NOT_PROVIDED.getValue());
+            var trace = NewRelic.getAgent().getTracedMethod();
+            trace.addCustomAttribute("invalidReasonRequestTimestampNotProvided", true);
+        }
+    }
+
+    private void saveInvalidCustomItemToS3(
+            ValidationResult validationResult
+    ) {
+        try {
+
+            logger.info("message:Putting invalid custom item into S3 bucket...");
+
+            var validationResultAsString = gson.toJson(validationResult);
+            logger.info("message:Validation result as string ->" + validationResultAsString);
+
+            var inputStream = new ByteArrayInputStream(
+                    validationResultAsString.getBytes());
+            logger.info("message:Converted to input stream.");
+
+            var putObjectRequest = new PutObjectRequest(
+                    "InvalidCustomItems",
+                    validationResult.getValidationId() + ".json",
+                    inputStream,
+                    new ObjectMetadata()
+            );
+            logger.info("message:PutObjectRequest is created.");
+
+            amazonS3.putObject(putObjectRequest);
+            logger.info("message:Invalid custom item is put into S3 bucket.");
+        }
+        catch (AmazonServiceException e) {
+            logger.info("message:Error is occurred by putting validation result into S3");
+            logger.error("message:" + e.getErrorMessage());
         }
     }
 }
